d45ef76e3adb0640533d4490b4ad79c9b94d6732
maksymnl <adw0988245131@gmail.com>
2023-07-22 11:05:17 +0300

    на проверку

--- src/index.js
+++ src/index.js
@@ -84,9 +84,11 @@ function onLoadMore() {
       if (currenHits <= 0) {
         Notify.failure(
           "We're sorry, but you've reached the end of search results."
-        );
+       );
         loadMore.classList.add('is-hiden');
       }
+     
     })
     .catch(error => console.log(error.message));
 }
+
